38974633b6c86e757a03c5c419ee04335677254e
danixoid <danixoid@gmail.com>
2015-03-12 14:34:48 +0600

    style

--- wp-content/themes/rambo/index-slides.php
+++ wp-content/themes/rambo/index-slides.php
@@ -14,8 +14,8 @@
 <div class="portfolio_main_content">	
 	<div class="container">	
 		<div class="row-fluid featured_port_title">
-			<h1>Featured Portfolio Projects</h1>
-			<p> Maecenas sit amet tincidunt elit. Pellentesque habitant morbi tristique senectus et netus et Nulla facilisi.</p>
+			<h1>Информационный тренинговый портал</h1>
+			<p>Единое тренинговое пространство  Perfecta</p>
 		</div>	
 		<div class="row">
             <div class="">
