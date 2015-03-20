c7747b2c52ef1a82c1aaa34f0af03b5fcbb86db8
danixoid <danixoid@gmail.com>
2015-03-20 10:19:15 +0600

    Посты

--- wp-content/themes/rambo/index.php
+++ wp-content/themes/rambo/index.php
@@ -21,17 +21,18 @@ $image_uri= WEBRITI_TEMPLATE_DIR_URI. '/images' ;
 					$more = 0;?>
 			<div class="blog_section" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="media">
-					<?php $defalt_arg =array('class' => "blog_section_img" )?>
-					<?php if(has_post_thumbnail()):?>
-					<a  href="<?php the_permalink(); ?>" class="pull-left blog_pull_img">
-					<?php the_post_thumbnail('blog1_section_img', $defalt_arg); ?>
-					</a>
-					<?php endif;?>
+
 					<div class="media-body">
 					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 					<span class="blog_tags"><h5><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) );?>"><?php the_author();?></a><span><?php echo get_the_date('M j,Y');?></span></h5></span>
 					<span class="blog_tags"><i class="fa fa-group"></i><?php the_category(',');?></span>
-					<p><?php  echo the_excerpt(); ?></p>
+					<?php $defalt_arg =array('class' => "blog_section_img" )?>
+					<?php if(has_post_thumbnail()):?>
+					<a  href="<?php the_permalink(); ?>" class="blog_pull_img">
+					<?php the_post_thumbnail('blog2_section_img', $defalt_arg); ?>
+					</a>
+					<?php endif;?>
+                    <p><?php  echo the_excerpt(); ?></p>
 					<?php $posttags = get_the_tags();?>
 					<p><?php if($posttags) { ?>
 					<span class="blog_tags"><i class="fa fa-tags"></i> 
